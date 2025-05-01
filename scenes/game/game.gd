4dcd17d955a385f2f02f010f11204b395e0e5267
Christian Toribio <chris09toribio@gmail.com>
2025-05-01 14:20:17 -0700

    added in enemy

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -2,6 +2,17 @@ class_name Game extends Node
 
 @export var game_map: GameMap
 @export var player: Player
+@export var enemy_container: Node
+
+var enemies: Array[Unit] = []
+
+@onready var goblin_scene: PackedScene = load("res://scenes/game/units/goblin/goblin.tscn")
 
 func _ready() -> void:
-	player.set_cell_position(game_map.get_origin_position() + Vector2i(1, 1))
+	player.set_cell_position(game_map.get_origin_position() + Vector2i(5, 3))
+
+	var goblin: Goblin = goblin_scene.instantiate()
+	goblin.game_map = game_map
+	enemy_container.add_child(goblin)
+	goblin.set_cell_position(game_map.get_origin_position() + Vector2i(8, 3))
+	goblin.sprite.flip_h = goblin.cell > player.cell
---
+++ scenes/game/units/goblin/goblin.gd
@@ -0,0 +1,5 @@
+class_name Goblin extends Unit
+
+@export var move_component: MoveComponent
+@export var attack_component: AttackComponent
+@export var health_component: HealthComponent
